1f04bc3d2f7bacd2505478f1b7a2a603320a02b1
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-09-22 10:18:47 +0700

    first commit

---
+++ ESP.py
@@ -0,0 +1,12 @@
+nilai = float(input("Masukkan nilai persentase: "))
+
+if nilai >= 90:
+    print("Excellent performance")
+elif nilai >= 80:
+    print("Very Good performance")
+elif nilai >= 70:
+    print("Good performance")
+elif nilai >= 60:
+    print("Average performance")
+else:
+    print("Below average performance")
