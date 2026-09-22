d12122276e56e7522566094b9052052761ede590
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-09-22 10:58:04 +0700

    Menambahkan beberapa tambahan data terkait print dan while true agar pengulangan bisa terjadi

--- ESP.py
+++ ESP.py
@@ -1,17 +1,25 @@
-nilai = float(input("Masukkan nilaimu Rakyatku: "))
+while True:
+    nilai = float(input("Masukkan nilaimu Rakyatku: "))
 
-if nilai >= 90:
-    print("Excellent performance")
-elif nilai >= 80:
-    print("Very Good performance")
-elif nilai >= 70:
-    print("Good performance")
-elif nilai >= 60:
-    print("Average performance")
-else:
-    print("Below average performance")
-    print("Belajar lagi ya Rakyatku, jangan menyerah!")
+    if nilai >= 90:
+        print("Excellent performance")
+        print("Selamat ya Rakyatku, kamu hebat!")
+    elif nilai >= 80:
+        print("Very Good performance")
+        print("Selamat ya Rakyatku, kamu hebat!")
+    elif nilai >= 70:
+        print("Good performance")
+        print("Semangat ya Rakyatku, kamu bisa lebih baik lagi!")
+    elif nilai >= 60:
+        print("Average performance")
+        print("Semangat ya Rakyatku, kamu bisa lebih baik lagi!")
+    else:
+        print("Below average performance")
+        print("Belajar lagi ya Rakyatku, jangan menyerah!")
 
-
+    ulang = input("Apakah Anda ingin memasukkan nilai lagi? (y/n): ")
+    if ulang.lower() != 'y':
+        print("Terima kasih ya rakyatku ini 50 $ untukmu, semoga bermanfaat")
+        break
     # finish
     # membuat program untuk menentukan kinerja berdasarkan nilai persentase yang dimasukkan oleh pengguna. Program ini menggunakan struktur percabangan if-elif-else untuk mengevaluasi nilai dan memberikan output yang sesuai.
