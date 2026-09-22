27776e166d8dbc6ce7ec01b59ebf681b8e53c07b
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-09-22 10:38:54 +0700

    menyelesaikan Pencarian 3 data terbesar

--- Search_3biggest_number.py
+++ Search_3biggest_number.py
@@ -11,3 +11,4 @@ else:
 
 print("Angka terbesar adalah:", terbesar)
 #catatan sudah menyelesaikan pencarian angka terbesar dari tiga angka yang dimasukkan oleh pengguna. Program ini menggunakan struktur percabangan if-elif-else untuk membandingkan ketiga angka dan menentukan angka terbesar di antara mereka.
+#finish
